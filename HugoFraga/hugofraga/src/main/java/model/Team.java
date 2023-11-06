221647957edd2b26170b23abadf86b92f6e49f73
Hugo Fraga <84546221+fragahugo1@users.noreply.github.com>
2023-11-06 18:08:01 -0300

    p1 de poo e java web

---
+++ HugoFraga/hugofraga/src/main/java/model/Team.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.util.List;
+
+public class Team {
+    private String name;
+    private String colors;
+    private List<Player> players;
+    
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getColors() {
+        return this.colors;
+    }
+    public void setColors(String colors) {
+        this.colors = colors;
+    }
+    public List<Player> getPlayers() {
+        return this.players;
+    }
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+}
